aef9f12dd724a89abfbb386a8884365830f4a4a4
Billy <billy@Billyui-MacBook-Pro.local>
2020-02-08 01:20:29 +0900

    Third commit

--- tumblbug_noflex.js
+++ tumblbug_noflex.js
@@ -1,20 +1,20 @@
- var image = document.getElementsByTagName('widgetimage')[0];
- var image_to_show = 0;
- var image_container = ['wave-3473335_1920.jpg','water-424807_1920.jpg','ocean-3605547_1920.jpg'];    //Put all the images you want
+ let image = document.getElementsByClassName('frontimage');
+ let image_to_show = 0;
+ let image_container = ['wave-3473335_1920.jpg','water-424807_1920.jpg','ocean-3605547_1920.jpg'];   
 
 
 setInterval(function(){
 
-if(image_to_show >= image_container.length -1)      //Return to the first one
+if(image_to_show >= image_container.length -1)     
 {
-    image_to_show = 0;  
-}
+     image_to_show = 0;  
+ }
 else
 {
     image_to_show++;
 }
 
-    widgetimage.src = image_container[image_to_show];
+    image = image_container[image_to_show];
 
 },3000);
 
